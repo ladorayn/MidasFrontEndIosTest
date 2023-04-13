2e78ed8bf41e3720b0701d2fbd4a29ed6f5ece30
ladorayn <lado.rayhan@ui.ac.id>
2023-04-13 16:05:46 +0700

    Initial Commit

---
+++ MidasFrontEndTest/MidasFrontEndTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MidasFrontEndTestApp.swift
+//  MidasFrontEndTest
+//
+//  Created by Lado Rayhan on 13/04/23.
+//
+
+import SwiftUI
+
+@main
+struct MidasFrontEndTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
